de556318a61cabe6bab565b7ebe8a712616d9106
Jiang Jun <jiang.jun@microcyber.cn>
2013-01-23 20:02:12 +0800

    use wfp login window

--- src/Gui/McsRemote.Control/Login.xaml.cs
+++ src/Gui/McsRemote.Control/Login.xaml.cs
@@ -19,9 +19,74 @@ namespace McsRemote.Control
     /// </summary>
     public partial class Login : Window
     {
+        private bool m_isLogin = false;
+        private GuiAccess.UserCli userMge = null;
+        public GuiAccess.UserCli UserManagement
+        {
+            get { return userMge; }
+            set { userMge = value; }
+        }
+        public bool IsLogin
+        {
+            get { return m_isLogin; }
+        }
+        private int m_nSession = 0;
+        public int Session
+        {
+            get { return m_nSession; }
+        }
+        private string m_strUserName = "";
+        public string UserName
+        {
+            get { return m_strUserName; }
+        }
+        private int m_nUserID = 0;
+        public int UserID
+        {
+            get { return m_nUserID; }
+        }
         public Login()
         {
             InitializeComponent();
+        }
+
+        private void UserLogin()
+        {
+            string strHash = GuiAccess.UserHash.HashUserInfo(this.tbUserName.Text,
+                this.pdbPassword.Password);
+
+            m_nSession = userMge.Login(this.tbUserName.Text, strHash);
+
+            if (m_nSession > 0)
+            {
+                m_strUserName = this.tbUserName.Text;
+                //m_nUserID = userMge.g
+                m_isLogin = true;
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Login failed, please check your user name and password.");
+            }
+        }
+
+        private void bnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            UserLogin();
+        }
+
+        private void bnExit_Click(object sender, RoutedEventArgs e)
+        {
+            userMge.Logout(m_nSession);
+            this.Close();
+        }
+
+        private void pdbPassword_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                UserLogin();
+            }
         }
     }
 }
